58fb8e94a07c114c8db605d392d198a1fc6fbade
Micaela <msberry9@gmail.com>
2017-05-27 13:48:27 -0400

    updated Virtual Pet

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,4 +1,64 @@
 
 public class VirtualPet {
 
+	String name;
+	int energyNow = 50;
+	int feedNow = 20;
+	int sleepNow = 30;
+	int pillageNow = 50;
+	
+	int getEnergyNow (){
+	return energyNow;
+	}
+	
+	int getFeedNow(){
+	return feedNow;}
+	
+	int getSleepNow(){
+	return sleepNow;}
+	
+	int getPillageNow(){
+	return pillageNow;}
+	
+	
+	void energy () {
+		energyNow = energyNow + 10;
+		sleepNow = sleepNow -10;
+		feedNow = feedNow -10;
+		pillageNow = pillageNow + 10;
+		System.out.println("Louie drank an energy drink and has more energy!\nBut his levels have changed.\n");
+	}
+	
+	void hunger() {
+		feedNow = feedNow + 10;
+		sleepNow = sleepNow - 5;
+		energyNow = energyNow - 10;
+		pillageNow = pillageNow - 10;
+		System.out.println("Louie ate some stinky food!\nBut his levels have changed.\n");
+	}
+
+	void sleeping() {
+		sleepNow = sleepNow + 5;
+		energyNow = energyNow + 10;
+		feedNow = feedNow -10;
+		pillageNow = pillageNow - 10;
+		System.out.println("Louie is getting some ZzzZzz!\nBut his levels have changed.\n");
+	}
+
+	void pillage() {
+		pillageNow = pillageNow + 10;
+		sleepNow = sleepNow - 5;
+		feedNow = feedNow + 10;
+		sleepNow = sleepNow - 10;
+		System.out.println("Louie travel the 7 seas and found some treasure!\nBut his levels have changed.\n");
+
+	}
+
+	void tick() {
+		energyNow = energyNow - 5;
+		feedNow = feedNow + 5;
+		sleepNow = sleepNow + 5;
+		pillageNow = pillageNow + 5;
+	}
+
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -7,12 +7,56 @@ public class VirtualPetApp {
 
 		Scanner input = new Scanner(System.in);
 
-		Dog fido = new Dog();
-		writeLine("My dog is " + fido);
+		VirtualPet pet = new VirtualPet();
 
-		fido.name = "Fido"; // always write name(fido) and give it a value(name)
-							// before asking for it's value
-		writeLine("Its name is " + fido.name);
+		pet.name = "Louie the Pirate Cat";
+
+		// add a loop for responses
+		System.out.println("Welcome to Virtual Pet Extravaganza! ");
+		do {
+			System.out.println("Here is the status of " + pet.name + "\nHunger is " + pet.feedNow + "\nTiredness is "
+					+ pet.sleepNow + "\nPillaging is " + pet.pillageNow + "\nEnergy is " + pet.getEnergyNow());
+
+			System.out.println("\nWhat would you like " + pet.name + " to do?\n Enter the number on the menu below\n");
+			System.out.println("1: Feed " + pet.name + " some stinky food.");
+			System.out.println("2: Make " + pet.name + " catch some ZzzZzz.");
+			System.out.println("3: Make " + pet.name + " sail the 7 seas and find treasure.");
+			System.out.println("4: Get " + pet.name + " energy level.");
+			String userInput = input.nextLine();
+
+			// this is where your do loop *was*
+			switch (userInput) {
+			case "1":
+				pet.hunger();
+				break;
+
+			case "2":
+				pet.sleeping();
+				break;
+
+			case "3":
+				pet.pillage();
+				break;
+
+			case "4":
+				pet.energy();
+				break;
+
+			}
+			pet.tick();
+
+		} while (pet.getEnergyNow() > 0);
+
+		if (pet.getEnergyNow() > 0) {
+			System.out.println("You made " + pet.name + " walk the \nplank to Davey Jones' locker.");
+		}
+		if (pet.getFeedNow() > 0) {
+			System.out.println("You made " + pet.name + " walk the \nplank to Davey Jones' locker.");
+		} if (pet.getPillageNow() > 0) {
+			System.out.println("You made " + pet.name + " walk the \nplank to Davey Jones' locker.");
+		}if (pet.getSleepNow() > 0) {
+			System.out.println("You made " + pet.name + " walk the \nplank to Davey Jones' locker.");
+		}
 	}
 
-}
+}// end main method
